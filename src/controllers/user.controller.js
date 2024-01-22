e1b85a9bf36681ab36666522de8d52fcbe822ce6
Bibek Regmi <1016matrixbibek@gmail.com>
2024-01-22 18:47:37 +0545

    Update cover image method added into user controller and response is  returned in the updateAccountDetails and updateUserAvatar methods

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -287,13 +287,43 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
     throw new ApiError(400, "Error while uploading avatar");
   };
 
-  await User.findByIdAndUpdate(req.user?._id, 
+  const user = await User.findByIdAndUpdate(req.user?._id, 
     {
       $set: {
         avatar: avatar.url
       }
     },
-    {new: true}).select("-password")
+    {new: true}).select("-password");
+
+    return res.status(200)
+    .json(new ApiResponse(200, user, "Avatar image uploaded successfully"));
+});
+
+
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+  const coverImageLocalPath = req.file?.path;
+
+  if(!coverImageLocalPath){
+    throw new ApiError(400, "Avatar Image missing")
+  };
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if(!coverImage.url){
+    throw new ApiError(400, "Error while uploading cover image");
+  };
+
+  const user = await User.findByIdAndUpdate(req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url
+      }
+    },
+    {new: true}).select("-password");
+
+    return res.status(200)
+    .json(new ApiResponse(200, user, "Cover image uploaded successfully"));
+
 });
 
 
